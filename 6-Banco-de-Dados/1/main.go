f750aaac1f7338ef2144987fd52859d7f4dac945
Felipe Yamaoka <fehyamaoka@gmail.com>
2024-06-10 23:14:39 -0300

    add: Banco de Dados - Simple Queries and GORM uses

--- 6-Banco-de-Dados/1/main.go
+++ 6-Banco-de-Dados/1/main.go
@@ -27,9 +27,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
-
 	product := NewProduct("Notebook", 1899.90)
 	err = insertProduct(db, product)
 	if err != nil {
@@ -40,11 +38,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	p, err := selectProduct(db, product.ID)
+	// p, err := selectProduct(db, product.ID)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+	products, err := selectAllProducts(db)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+	for _, p := range products {
+		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
+	err = deleteProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -85,4 +94,35 @@ func selectProduct(db *sql.DB, id string) (*Product, error) {
 		return nil, err
 	}
 	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
+}
+
+func deleteProduct(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("delete from products where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
